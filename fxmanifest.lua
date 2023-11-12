8a3e45eb2e9931e8e33df41d842af07812d4129d
DefinitelyNotNoah <35615266+DefinitelyNotNoah@users.noreply.github.com>
2023-11-11 19:01:26 -0600

    Add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
+
+mono_rt2 'Prerelease expiring 2023-06-30. See https://aka.cfx.re/mono-rt2-preview for info.'
+
+client_script 'bin/Client.net.dll'
+server_script 'bin/Server.net.dll'
