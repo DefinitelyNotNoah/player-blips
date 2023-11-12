69583266191ae2d2c085c90a6b4817d79a189c8c
DefinitelyNotNoah <35615266+DefinitelyNotNoah@users.noreply.github.com>
2023-11-12 00:20:12 -0600

    Add config.json
    The update rate for coordinate blips can now be adjusted in real-time.

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using CitizenFX.Core;
 using CitizenFX.Server;
 using CitizenFX.Server.Native;
+using Newtonsoft.Json;
 
 namespace Server;
 
@@ -13,7 +15,7 @@ public class ServerScript : BaseScript
     private void SendPlayerData()
     {
         IDictionary<string, object> playerData = new Dictionary<string, object>();
-            
+
         // Loop through all the players known to the server, where their ped entity exists.
         foreach (Player player in new PlayerList().ToList().Where(player => player.Character != null))
         {
@@ -28,35 +30,39 @@ public class ServerScript : BaseScript
                 Natives.GetEntityModel(vehicle),
                 Natives.GetEntityRoutingBucket(player.Character.Handle)
             };
-            
+
             // Update player state bags to make routing buckets known client-side.
             player.State["bucket"] = Natives.GetEntityRoutingBucket(player.Character.Handle);
         }
+
         Events.TriggerAllClientsEvent("PlayerBlips-Client:ReceivePlayerData", playerData);
     }
-        
+
     [EventHandler("playerEnteredScope")]
     public void PlayerEnteredScopeServerEvent(IDictionary<string, object> data)
     {
         Events.TriggerAllClientsEvent("PlayerBlips-Client:PlayerEnteredScope", data["for"], data["player"]);
     }
-        
+
     [EventHandler("playerLeftScope")]
     public void PlayerLeftScopeServerEvent(IDictionary<string, object> data)
     {
         Events.TriggerAllClientsEvent("PlayerBlips-Client:PlayerLeftScope", data["for"], data["player"]);
     }
-        
+
     [EventHandler("playerDropped")]
     public void PlayerDroppedServerEvent([Source] Player source, string reason)
     {
         Events.TriggerAllClientsEvent("PlayerBlips-Client:PlayerDropped", source.Handle);
     }
-        
+
     [Tick]
     public async Coroutine OnTick()
     {
         SendPlayerData();
-        await Wait(500);
+        string config = File.ReadAllText(Natives.GetResourcePath("playerblips") + "/config.json");
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(config);
+        
+        await Wait((uint)json["Delay"]);
     }
 }
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,5 +3,7 @@ game 'gta5'
 
 mono_rt2 'Prerelease expiring 2023-06-30. See https://aka.cfx.re/mono-rt2-preview for info.'
 
+file 'bin/Newtonsoft.Json.dll'
+
 client_script 'bin/Client.net.dll'
 server_script 'bin/Server.net.dll'
